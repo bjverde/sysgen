1df30c2b86122744f77d7f829a060283956f01d2
reinaldo.junior <reinaldo.junior@m50879.mpdft.gov.br>
2018-03-19 16:43:38 -0300

    :hammer: nova constante

--- includes/constantes.php
+++ includes/constantes.php
@@ -12,5 +12,6 @@ define('SYSTEM_VERSION', 'v 0.3.0');
 define('APLICATIVO',SYSTEM_ACRONYM);
 define('DS'    , DIRECTORY_SEPARATOR);
 define('ROOT_PATH'    , '../');
+define('CHAR_MAX_TEXT_FIELD'    , 100);
 
 ?>
