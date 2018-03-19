5ae88b58f554a7756d1c48d402ddb00b3b8b9f89
Bjverde <bjverde@yahoo.com.br>
2018-03-19 00:37:43 -0300

    :memo: documentação

--- ajuda/info_gen00_pt-br.php
+++ ajuda/info_gen00_pt-br.php
@@ -1,5 +1,7 @@
 <h3>Orientações sobre a estrutura de banco de dados</h3>
 
+<br> Essa orientações não são obrigações ! Se seguir terá menos trabalho e para arrumar o codigo gerado.
+
 <ul>
 	<li>Evite chaves primarias compostas. Procure utilizar sempre chaves simples com autoincremente. Transforme as chaves compostas em chaves candiadatas com unique key.</li>
 	<li>Faça descrições do campos das tabela, assim eles poderam virá ajuda</li>
