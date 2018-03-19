667a43e74389525d4cf615bd203231f4b47a9ef8
reinaldo.junior <reinaldo.junior@m50879.mpdft.gov.br>
2018-03-19 13:32:38 -0300

    :memo: Documentação

--- ajuda/info_gen00_pt-br.php
+++ ajuda/info_gen00_pt-br.php
@@ -1,14 +1,14 @@
 <h3>Orientações sobre a estrutura de banco de dados</h3>
 
-<br> Essa orientações não são obrigações ! Se seguir terá menos trabalho e para arrumar o codigo gerado.
+<br> Essas orientações não são obrigações! Se seguir terá menos trabalho e para arrumar o código gerado.
 
 <ul>
-	<li>Evite chaves primarias compostas. Procure utilizar sempre chaves simples com autoincremente. Transforme as chaves compostas em chaves candiadatas com unique key.</li>
-	<li>Faça descrições do campos das tabela, assim eles poderam virá ajuda</li>
+	<li>Evite chaves primarias compostas. Procure utilizar sempre a chaves primaria simples com AUTOINCREMENT. Transforme as chaves compostas em chaves candidatas com Unique Key. Veja mais informações em <a href="https://www.devmedia.com.br/forum/qual-a-diferenca-entreprimary-key-e-unique-key/513038" target="_blank">Qual a diferença entre Primary Key e Unique Key ?</a></li>
+	<li>Faça descrições dos campos da tabela, assim eles poderão virá ajuda</li>
 </ul>
 
 <h3>Campos:</h3>
 <ul>
-	<li>Sigla do Sistema: A sigla do sistema é o ID do sistema. Será utiliza para criar a pasta do novo sistema. Por padrão irá aparecer no canto superior esquerdo</li>
-	<li>Nome do sistem: É descrição completa do nome do sistema ou o que significa a sigla</li>
+	<li>Sigla do Sistema: A sigla do sistema é o ID do sistema. Será utiliza para criar a pasta do novo sistema. Por padrão aparecerá no canto superior esquerdo</li>
+	<li>Nome do sistema: É descrição completa do nome do sistema ou o que significa a sigla</li>
 </ul>
