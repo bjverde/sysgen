3ad88f993fe3c719883f8b017a8602c1a29735c5
Bjverde <bjverde@yahoo.com.br>
2018-03-19 01:25:18 -0300

    :memo: documentação

--- ajuda/info_gen00_pt-br.php
+++ ajuda/info_gen00_pt-br.php
@@ -6,3 +6,9 @@
 	<li>Evite chaves primarias compostas. Procure utilizar sempre chaves simples com autoincremente. Transforme as chaves compostas em chaves candiadatas com unique key.</li>
 	<li>Faça descrições do campos das tabela, assim eles poderam virá ajuda</li>
 </ul>
+
+<h3>Campos:</h3>
+<ul>
+	<li>Sigla do Sistema: A sigla do sistema é o ID do sistema. Será utiliza para criar a pasta do novo sistema. Por padrão irá aparecer no canto superior esquerdo</li>
+	<li>Nome do sistem: É descrição completa do nome do sistema ou o que significa a sigla</li>
+</ul>
