0e6c4b0ead4c67f6e5709ed07a0ba82c57f60b19
Bjverde <bjverde@yahoo.com.br>
2018-03-20 10:42:11 -0300

    :memo: novas mensagens autoload e redirect

--- classes/Message.class.php
+++ classes/Message.class.php
@@ -13,9 +13,11 @@ final class Message {
 	const GEN02_MKDIR_SYSTEM = 'Criada pasta base ';
 	const GEN02_COPY_SYSTEM_SKELETON    = 'Copiado estrutura básica de pastas.';
 	const GEN02_CREATED_CONSTANTS       = 'Criado arquivo de constantes.';
+	const GEN02_CREATED_AUTOLOAD        = 'Criado arquivo de autoload do sistema.';
 	const GEN02_CREATED_CONFIG_DATABASE = 'Criado arquivo de configuração de banco.';
 	const GEN02_CREATED_MENU  = 'Criado arquivo de menu.';
 	const GEN02_CREATED_INDEX = 'Criado arquivo index do sistema.';
+	const GEN02_REDIRECT_STEP03 = 'Você está na etapa 03 de 03. Essa parte pode demorar. Quanto maior o número de tabelas maior a demora.';
 	
 	const GEN03_TITLE         = 'Etapa 03 de 03';
 	const GEN03_NEW_SYSTEM_OK = 'Sistema Criado com Sucesso';
