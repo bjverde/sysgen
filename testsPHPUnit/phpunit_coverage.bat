4466b4d2427b47f4fa7eefdf987131fd9b2023ad
Bjverde <bjverde@yahoo.com.br>
2018-07-11 01:50:59 -0300

    :white_check_mark: novo teste

--- testsPHPUnit/phpunit_coverage.bat
+++ testsPHPUnit/phpunit_coverage.bat
@@ -1,13 +1,16 @@
-REM 4.8.23 Temp Wamp
-D:\wamp\bin\php\php5.6.31\php.exe D:\wamp\bin\php\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\sysgen\testsPHPUnit
-
 REM 4.8.23 Final
-REM D:\wamp\bin\php\php5.6.31\php.exe D:\wamp\bin\php\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\base\testsPHPUnit\
+D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\php\phpunit\phpunit-4.8.36.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\sysgen\testsPHPUnit
 
-
-
-REM 5.7.23 Temp Wamp
-REM D:\wamp\bin\php\php5.6.31\php.exe D:\wamp\bin\php\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\base\testsPHPUnit\
 
 REM 5.7.23 Final
-REM D:\wamp\bin\php\php5.6.31\php.exe D:\wamp\bin\php\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\base\testsPHPUnit\
+REM D:\wamp\bin\php\php5.6.35\php.exe D:\wamp\bin\php\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\sysgen\testsPHPUnit
+
+REM 5.7.23 Final
+REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\php\phpunit\phpunit-5.7.23.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\sysgen\testsPHPUnit
+
+
+REM 6.4.3 Final
+REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\php\phpunit\phpunit-6.4.3.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\sysgen\testsPHPUnit
+
+REM 7.2.4 Final
+REM D:\wamp\bin\php\php7.1.16\php.exe D:\wamp\bin\php\phpunit\phpunit-7.2.4.phar --coverage-html D:\wamp\tmp\cover D:\wamp\www\formDin\sysgen\testsPHPUnit
