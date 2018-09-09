fc19b57629c1a8ed49e8060f9aab161a4322b939
Bjverde <bjverde@yahoo.com.br>
2018-09-08 23:21:08 -0300

    fixed #5

--- classes/TCreateForm.class.php
+++ classes/TCreateForm.class.php
@@ -233,6 +233,7 @@ class TCreateForm
         if (ArrayHelper::has('COLUMN_COMMENT', $this->listColumnsProperties)) {
             $COLUMN_COMMENT = $this->listColumnsProperties['COLUMN_COMMENT'][$key];
             if (!empty($COLUMN_COMMENT)) {
+                $COLUMN_COMMENT = str_replace("'","",$COLUMN_COMMENT);
                 $this->addLine('$frm->getLabel(\''.$fieldName.'\')->setToolTip(\''.$COLUMN_COMMENT.'\');');
             }
         }
