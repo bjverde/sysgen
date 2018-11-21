5781588274e3a608c34cf424800331fc0cc33e81
Bjverde <bjverde@yahoo.com.br>
2018-11-20 23:15:31 -0200

    :hammer: #13 inclusão do changelog

---
+++ system_skeleton/ajuda/changelog.php
@@ -0,0 +1,19 @@
+<?php
+require_once("../includes/constantes.php");
+$msgSysNameVersion = SYSTEM_NAME.' - v'.SYSTEM_VERSION;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Changelog - <?php echo($msgSysNameVersion); ?></title>
+</head>
+<body>
+    <h2><?php echo($msgSysNameVersion); ?></h2>
+    <li>versão 0.0.0</li>
+        <ul>
+            <li>Primeira versão</li>
+        </ul>
+</body>
+</html>
