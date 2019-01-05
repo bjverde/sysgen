aec1358f19b6a31d95845eddee8c66a052452c2c
Bjverde <bjverde@yahoo.com.br>
2019-01-05 13:42:02 -0200

    :bug: Fixed #32
    correção da imagem do menu

--- includes/menu.php
+++ includes/menu.php
@@ -12,7 +12,7 @@
 
 $menu = new TMenuDhtmlx();
 //$menu->add('1', null, 'Menu', null, null, 'menu-alt-512.png');
-$menu->add('10', null, 'Gerador', 'gen00.php', null, 'settings_tool_preferences-512');
+$menu->add('10', null, 'Gerador', 'gen00.php', null, 'settings_tool_preferences-512.png');
 
 
 $menu->getXml();
