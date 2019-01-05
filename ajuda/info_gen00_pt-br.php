060c7d1641a24201a2e8be16d6e8f8c7785d86ee
Bjverde <bjverde@yahoo.com.br>
2019-01-05 19:31:50 -0200

    :memo: melhoria da tela inicial

--- ajuda/info_gen00_pt-br.php
+++ ajuda/info_gen00_pt-br.php
@@ -1,10 +1,11 @@
 <h3>Orientações sobre a estrutura de banco de dados</h3>
-
-<br> Essas orientações não são obrigações! Se seguir terá menos trabalho e para arrumar o código gerado.
-
+<br> Sigua as 4 dicas abaixo e será mais feliz ! Essas orientações não são obrigatorias, seguindo o código gerado irá funcionar de forma mais facil. 
+<a href="https://github.com/bjverde/formDin/wiki/Usando-DAO-e-VO#dicas-para-modelar-o-banco-de-dados" target="_blank">Veja na Wiki do formDin em detalhes cada dica.</a>
 <ul>
-	<li>Evite chaves primarias compostas. Procure utilizar sempre a chaves primaria simples com AUTOINCREMENT. Transforme as chaves compostas em chaves candidatas com Unique Key. Veja mais informações em <a href="https://www.devmedia.com.br/forum/qual-a-diferenca-entreprimary-key-e-unique-key/513038" target="_blank">Qual a diferença entre Primary Key e Unique Key ?</a></li>
-	<li>Faça descrições dos campos da tabela, assim eles poderão virá ajuda</li>
+	<li>NÃO use chaves primárias composta !! Evite chaves primarias compostas. Procure utilizar sempre a chaves primaria simples com AUTOINCREMENT. Transforme as chaves compostas em chaves candidatas com Unique Key.</li>
+	<li>Procure criar chaves primárias (PK) com nomes únicos. Nada de toda PK com o nome ID</li>
+	<li>Use VIEWS e/ou SubSelects</li>
+	<li>Use tabelas de apoio, para os "tipos"</li>
 </ul>
 
 <h3>Campos:</h3>
