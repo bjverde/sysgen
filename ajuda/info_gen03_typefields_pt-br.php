45943f2d91b1eef433c5f663ef7a73abc9469dc0
Bjverde <bjverde@yahoo.com.br>
2019-03-28 01:31:21 -0300

    :hammer: #25 change text help

--- ajuda/info_gen03_typefields_pt-br.php
+++ ajuda/info_gen03_typefields_pt-br.php
@@ -3,5 +3,5 @@
 <ul>
 	<li>Select Field: é tipo mais simples é comum. É campo select igual o que aparece no grid</li>
 	<li>Autocomplete: Terá um campo texto que servirá de busca para buscar um item</li>
-	<li>Search On-line: Semelhante ao autocomplete porém a busca pode ser feita por diversos campos</li>
+	<!-- <li>Search On-line: Semelhante ao autocomplete porém a busca pode ser feita por diversos campos</li> -->
 </ul>
