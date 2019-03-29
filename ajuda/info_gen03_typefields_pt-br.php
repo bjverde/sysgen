29aab88a7beec7a60e060d431caa9f3fe5f9b3dc
Bjverde <bjverde@yahoo.com.br>
2019-03-29 03:09:51 -0300

    :memo: #26 melhoria da mensagem

--- ajuda/info_gen03_typefields_pt-br.php
+++ ajuda/info_gen03_typefields_pt-br.php
@@ -1,5 +1,5 @@
 <h3>Escolha um tipo de campo para a FK:</h3>
-<br>No grid abaixo cada coluna da tabela/view faz rerencia a outro campo. Na tela será gerado o tipo de campo para FK conforme selecionado 
+<br>No grid abaixo cada coluna da tabela/view faz rerencia a outro campo. Na tela será gerado o tipo de campo para FK conforme selecionado. <b>ATENÇÃO</b> Somente o SELECT irá funcionar logo na primeira versão. Nos outros tipos de campos será gerado um SCAFFOLD, com uma pequena intervenção irá funcionar
 <ul>
 	<li>Select Field: é tipo mais simples é comum. É campo select igual o que aparece no grid</li>
 	<li>Autocomplete: Terá um campo texto que servirá de busca para buscar um item</li>
