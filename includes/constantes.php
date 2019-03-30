4831f66c25f8cc9a7018385b3c93a0bcd275da79
Bjverde <bjverde@yahoo.com.br>
2019-03-30 14:54:43 -0300

    :memo: change version

--- includes/constantes.php
+++ includes/constantes.php
@@ -14,7 +14,7 @@ $debug = false;
 define('SYSTEM_NAME', 'Gerador de Sistemas em FormDin');
 define('SYSTEM_NAME_SUB', 'write less, do more. But "Talk is Cheap. Show me the Code"! ');
 define('SYSTEM_ACRONYM', 'sysGen');
-define('SYSTEM_VERSION', '1.1.0');
+define('SYSTEM_VERSION', '1.2.0-alpha');
 define('APLICATIVO', SYSTEM_ACRONYM);
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_PATH', '../');
