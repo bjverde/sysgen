1429c1938449c9b8d69fef51d3618910a1d830f6
Bjverde <bjverde@yahoo.com.br>
2019-03-30 12:41:34 -0300

    :hammer: #54 all files in project respeting PSR-1 and PSR-2

--- classes/autoload_sysgen.php
+++ classes/autoload_sysgen.php
@@ -10,7 +10,8 @@
  * PHP Version 5.6
  */
 if (!function_exists('sysgen_autoload')) {
-    function sysgen_autoload($class_name){
+	function sysgen_autoload($class_name)
+	{
 		$path = __DIR__.DS.$class_name.'.class.php';
 		if (file_exists($path)){
 			require_once $path;
