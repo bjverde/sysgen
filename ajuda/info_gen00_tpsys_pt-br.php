18de0ccb64687c723c4456be95809ca4e77be1ff
Bjverde <bjverde@yahoo.com.br>
2019-04-01 21:52:50 -0300

    :memo: #49 melhorando aviso de uso

--- ajuda/info_gen00_tpsys_pt-br.php
+++ ajuda/info_gen00_tpsys_pt-br.php
@@ -1,3 +1,6 @@
+<b>ATENÇÃO:</b> Para API REST é preciso habilitar a mod_rewrite no Apache. 
+<a href="https://github.com/bjverde/formDin/wiki/API-REST">Informações sobre no Wiki do FormDin</a> 
+
 <ul>
 	<li>Somente tela FormDin: Padrão FormDin com a geração de menus, dao, vo, negocio e tela</li>
 	<li>Somente API REST: Padrão RESTFull via api Json. Não tem menus e telas. tudo irá responder no SISTEMA/API</li>
