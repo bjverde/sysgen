3fa16037841f4cac8f3db0018e40c74f6ba44a0f
Bjverde <bjverde@yahoo.com.br>
2019-04-03 22:02:07 -0300

    :abc: removendo comentario

--- classes/TGeneratorHelper.class.php
+++ classes/TGeneratorHelper.class.php
@@ -419,7 +419,6 @@ class TGeneratorHelper
         $dao = self::getTDAOConect($table, $tableSchema);
         $listFieldsTable = $dao->loadFieldsOneTableFromDatabase();
         foreach ($listFieldsTable['DATA_TYPE'] as $key => $dataType) {
-            //$formDinType = self::convertDataType2FormDinType($dataType);
             $formDinType = TCreateForm::convertDataType2FormDinType($dataType);
             $listFieldsTable[TCreateForm::FORMDIN_TYPE_COLUMN_NAME][$key] = $formDinType;
             $fkTypeScreenReferenced = self::getFKTypeScreenReferencedSelected($table, $tableSchema, $listFieldsTable, $key);
