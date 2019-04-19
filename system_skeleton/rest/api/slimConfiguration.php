1195146bc34e4345d7cc695595e8dd1bcc159e6f
Bjverde <bjverde@yahoo.com.br>
2019-04-19 02:42:10 -0300

    :hammer: #63 retornando tudo em JSON

--- system_skeleton/rest/api/slimConfiguration.php
+++ system_skeleton/rest/api/slimConfiguration.php
@@ -6,5 +6,46 @@ function slimConfiguration(): \Slim\Container
             'displayErrorDetails' => getenv('DISPLAY_ERRORS_DETAILS'),
         ],
     ];
-    return new \Slim\Container($configuration);
+    $container = new \Slim\Container($configuration);
+
+    /**
+     * Converte os Exceptions entro da Aplicação em respostas JSON
+     */
+    $container['errorHandler'] = function ($container) {
+        return function ($request, $response, $exception) use ($container) {
+            $statusCode = $exception->getCode() ? $exception->getCode() : 500;
+            return $container['response']
+            ->withStatus($statusCode)
+            ->withHeader('Content-Type', 'Application/json')
+            ->withJson(["message" => $exception->getMessage()], $statusCode);
+        };
+    };
+    
+    /**
+     * Converte os Exceptions de Erros 405 - Not Allowed
+     */
+    $container['notAllowedHandler'] = function ($container) {
+        return function ($request, $response, $methods) use ($container) {
+            return $container['response']
+            ->withStatus(405)
+            ->withHeader('Allow', implode(', ', $methods))
+            ->withHeader('Content-Type', 'Application/json')
+            ->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
+            ->withJson(["message" => "Method not Allowed; Method must be one of: " . implode(', ', $methods)], 405);
+        };
+    };    
+    
+    /**
+     * Converte os Exceptions de Erros 404 - Not Found
+     */
+    $container['notFoundHandler'] = function ($container) {
+        return function ($request, $response) use ($container) {
+            return $container['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'Application/json')
+            ->withJson(['message' => 'Page not found']);
+        };
+    };
+    
+    return $container;
 }
