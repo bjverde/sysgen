4caf8079c897b98d333c9819f1489d343bc461d5
Bjverde <bjverde@yahoo.com.br>
2022-07-30 16:26:39 -0300

    :hammer: #77

--- ajuda/info_gen00_tpsys_pt-br.php
+++ ajuda/info_gen00_tpsys_pt-br.php
@@ -1,8 +1,8 @@
-<b>ATENÇÃO:</b> Para API REST é preciso habilitar a mod_rewrite no Apache. 
+<b>ATENÇÃO:</b> Para API REST é preciso habilitar a mod_rewrite no Apache do servidor de destino.
 <a href="https://github.com/bjverde/formDin/wiki/API-REST">Informações sobre no Wiki do FormDin</a> 
 
 <ul>
 	<li>Somente tela FormDin: Padrão FormDin com a geração de menus, dao, vo, negocio e tela</li>
 	<li>Somente API REST: Padrão RESTFull via api Json. Não tem menus e telas. tudo irá responder no SISTEMA/API</li>
-	<li>FormDin + REST: É descrição completa do nome do sistema ou o que significa a sigla</li>
+	<li>FormDin + REST: Terá as duas funcionalidades acima descritas</li>
 </ul>
